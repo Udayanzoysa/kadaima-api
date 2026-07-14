56aa7d31fbd240f7aebe9e4c516c1b094fbfb827
Udayanzoysa <unzoysa.un@gmail.com>
2026-07-15 04:09:09 +0530

    feat: initialize NestJS application with Swagger, CORS, and global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
+  await app.listen(8080, '0.0.0.0');
 }
 bootstrap();
